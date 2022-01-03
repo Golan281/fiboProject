ca98ff711579d8c5a089b5162860026ac2e76bce
Golan Blumenkrantz <golan281@gmail.com>
2022-01-03 13:28:27 +0200

    deleted duplicates

--- js-promise-ex3/3.js
+++
@@ -1,9 +0,0 @@
-/**
- * Create a promise (created here), that resolves with another promise,
- * that resolves with the string value "Nested promise value"
- */
-const myPromise = new Promise((resolve, reject) => {
-  // Your code here
-});
-
-export default myPromise;
