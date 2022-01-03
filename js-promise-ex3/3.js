5f8d78fbe07de256f7a3eed70ec8aa646ab3e28a
Golan Blumenkrantz <golan281@gmail.com>
2022-01-03 13:17:15 +0200

    push promise ex3

---
+++ js-promise-ex3/3.js
@@ -0,0 +1,9 @@
+/**
+ * Create a promise (created here), that resolves with another promise,
+ * that resolves with the string value "Nested promise value"
+ */
+const myPromise = new Promise((resolve, reject) => {
+  // Your code here
+});
+
+export default myPromise;
